e1562fb31e1e559006c0038e04aa152b4fd389d9
Lex 26 <>
2026-02-14 16:57:47 +0000

    update for pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/farmrpg-toolkit/',
+  base: '/',
   test: {
     include: ['src/**/*.test.{ts,tsx}'],
   },
